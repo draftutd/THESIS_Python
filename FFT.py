048b823efbed388276af26fda0e79bd9d51b6cf7
Peerapong Pimup <draftutd@hotmail.com>
2020-05-30 15:39:22 +0700

    draft

---
+++ FFT.py
@@ -0,0 +1,36 @@
+import read_data
+import numpy as np
+import matplotlib.pyplot as plt 
+
+Time,Data = read_data.read_data()
+
+samplingFrequency   = 1000;
+figure, axis = plt.subplots(2, 1)
+plt.subplots_adjust(hspace=1)
+
+x = Time 
+y = Data
+
+# Time domain representation of the resultant sine wave
+axis[0].plot(x, y)   
+axis[0].set_xlabel('TIME') 
+axis[0].set_ylabel('G') 
+axis[0].set_title('Time domain')
+
+# Frequency domain representation
+fourierTransform = np.fft.fft(Data)/len(Data)           # Normalize amplitude
+fourierTransform = fourierTransform[range(int(len(Data)/2))] # Exclude sampling frequency
+tpCount     = len(Data)
+values      = np.arange(int(tpCount/2))
+timePeriod  = tpCount/samplingFrequency
+frequencies = values/timePeriod
+
+# Frequency domain representation
+axis[1].set_title('Fourier transform depicting the frequency components')
+axis[1].plot(frequencies, abs(fourierTransform))
+axis[1].set_xlabel('Frequency')
+axis[1].set_ylabel('Amplitude')
+
+
+
+plt.show() 
--- read_data.py
+++ read_data.py
@@ -1,20 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt 
 Time = []
 Data = []
-dataframe = pd.read_excel (r'Data.xlsx')
-i =0 
-while i < 16384:
-    Time.append(dataframe['Time '][i])
-    Data.append(dataframe['g'][i])
-    i += 1
-    
-x = Time 
-y = Data
-print(len(y))
-plt.plot(x, y)   
-plt.xlabel('TIME') 
-plt.ylabel('G') 
-plt.title('Time domain') 
-plt.show() 
+
+def read_data():
+    dataframe = pd.read_excel (r'Data.xlsx')
+    i =0 
+    while i < 16384:
+        Time.append(dataframe['Time '][i])
+        Data.append(dataframe['g'][i])
+        i += 1
+    return Time,Data
+
 
