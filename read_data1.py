bc9358d207381b21cc0210e4485c4ad674c8f2cb
Peerapong Pimup <draftutd@hotmail.com>
2020-08-05 10:33:36 +0700

    draft

---
+++ read_data1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+Time = []
+Data = []
+
+def read_data():
+    dataframe = pd.read_excel (r'Data1.xlsx')
+    i =0 
+    while i < 6400:
+        Time.append(dataframe['Time '][i])
+        Data.append(dataframe['g'][i])
+        i += 1
+    return Time,Data
+
