c9b2ce6f5f0cc36274f2eda4cf4879441470b719
Peerapong Pimup <draftutd@hotmail.com>
2020-10-02 15:49:16 +0700

    draftutfd

---
+++ 2.10.2563.1.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+samplingFrequency   = 8000; #คูณ2.5เท่า
+figure, axis = plt.subplots(2, 1)
+plt.subplots_adjust(hspace=1)
+Time = []
+Data = []
+velocity = []
+dataframe = pd.read_excel (r'Data.xlsx')
+i =0 
+while i < 16384:
+    Time.append(dataframe['Time '][i])
+    Data.append(dataframe['g'][i])
+    i += 1
+
+
+def export(x,y):
+     f = open('datalog.txt', 'a')
+     f.write('%s : %s\n'%(x,y))
+     f.close()
+
+# Time domain representation of the resultant sine wave
+axis[0].plot(Time, Data)   
+axis[0].set_xlabel('TIME') 
+axis[0].set_ylabel('G') 
+axis[0].set_title('Time domain')
+print(Data)
+# Frequency domain representation
+fourierTransform = np.fft.fft(Data)/len(Data)           # Normalize amplitude
+fourierTransform = fourierTransform[range(int(len(Data)/2))] # Exclude sampling frequency
+tpCount     = len(Data)
+values      = np.arange(int(tpCount/2))
+timePeriod  = tpCount/samplingFrequency
+frequencies = values/timePeriod
+print(abs(fourierTransform))
+
+for x, y in zip(frequencies, abs(fourierTransform)):
+            if(x>=2 and x<=1000):
+                        velocity1=(y*9806.65)/(2*np.pi*x)
+                        velocity1=velocity1/0.7071067811 #คูณรูท2
+            else:
+                        velocity1=0
+            velocity.append(velocity1)
+            export(x,velocity1)
+
+
+# Frequency domain representation
+axis[1].set_title('Fourier transform depicting the frequency components')
+axis[1].plot(frequencies, velocity)
+axis[1].set_xlabel('Frequency')
+axis[1].set_ylabel('Amplitude')
+plt.show() 
+
--- 2.10.2563.py
+++ 2.10.2563.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-samplingFrequency   = 6400;
+samplingFrequency   = 8000;
 figure, axis = plt.subplots(2, 1)
 plt.subplots_adjust(hspace=1)
 Time = []
 Data = []
-velocity = [0]
+velocity = []
 dataframe = pd.read_excel (r'Data.xlsx')
 i =0 
 while i < 16384:
     Time.append(dataframe['Time '][i])
-    Data.append(dataframe['g'][i]*9.8)
+    Data.append(dataframe['g'][i])
     i += 1
 
 # Time domain representation of the resultant sine wave
@@ -20,23 +20,26 @@ axis[0].set_xlabel('TIME')
 axis[0].set_ylabel('G') 
 axis[0].set_title('Time domain')
 
-acceleration = Data
-time = 0.0001220703 #10Hz
-for acc in acceleration:
-    velocity.append(velocity[-1] + acc * time)
-del velocity[0]
 # Frequency domain representation
-fourierTransform = np.fft.fft(velocity)/len(velocity)           # Normalize amplitude
-fourierTransform = fourierTransform[range(int(len(velocity)/2))] # Exclude sampling frequency
-tpCount     = len(velocity)
+fourierTransform = np.fft.fft(Data)/len(Data)           # Normalize amplitude
+fourierTransform = fourierTransform[range(int(len(Data)/2))] # Exclude sampling frequency
+tpCount     = len(Data)
 values      = np.arange(int(tpCount/2))
 timePeriod  = tpCount/samplingFrequency
+
 frequencies = values/timePeriod
 
 
+for x, y in zip(frequencies, abs(fourierTransform)):
+            if(x>=2 and x<=1000):
+                        velocity1=(y*9806.65)/(2*np.pi*x)
+            else:
+                        velocity1=0
+            velocity.append(velocity1)
+
 # Frequency domain representation
 axis[1].set_title('Fourier transform depicting the frequency components')
-axis[1].plot(frequencies, abs(fourierTransform)*10)
+axis[1].plot(frequencies, velocity)
 axis[1].set_xlabel('Frequency')
 axis[1].set_ylabel('Amplitude')
 plt.show() 
