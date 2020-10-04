3df545a57caf10e172bd6be57ce3cf001c5e09b7
Peerapong Pimup <draftutd@hotmail.com>
2020-10-04 11:21:00 +0700

    draftutd

---
+++ hanning window.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from numpy.fft import fft, fftshift
+import numpy as np
+
+window = np.hanning(51)
+print(len(window))
+print(window)
+#plt.plot(window)
+#plt.title("Hann window")
+#plt.ylabel("Amplitude")
+#plt.xlabel("Sample")
+plt.figure()
+
+A = fft(window, 2048) / 25.5
+mag = np.abs(fftshift(A))
+freq = np.linspace(-0.5, 0.5, len(A))
+with np.errstate(divide='ignore', invalid='ignore'):
+    response = 20 * np.log10(mag)
+response = np.clip(response, -100, 100)
+
+print(len(freq))
+print(freq)
+print(len(response))
+print(response)
+plt.plot(freq, response)
+plt.title("Frequency response of the Hann window")
+plt.ylabel("Magnitude [dB]")
+plt.xlabel("Normalized frequency [cycles per sample]")
+plt.axis('tight')
+
+plt.show()
